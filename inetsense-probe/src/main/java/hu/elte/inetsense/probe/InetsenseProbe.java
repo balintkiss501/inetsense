bad43fe056934411ee2a36b7e300ad6c73c98a30
Szulyák Ákos <szulyak@client-137-164.wifi.elte.hu>
2016-04-13 17:01:19 +0200

    directory refact.

---
+++ inetsense-probe/src/main/java/hu/elte/inetsense/probe/InetsenseProbe.java
@@ -0,0 +1,25 @@
+
+package hu.elte.inetsense.probe;
+
+import hu.elte.inetsense.probe.speedtester.SpeedTester;
+import hu.elte.inetsense.probe.uploader.DataUploader;
+
+/**
+ * 
+ * Entry point.
+ * 
+ * @author Adam Kecskes
+ */
+public class InetsenseProbe {
+
+    
+    public static void main(String[] args) {
+        
+        DataUploader du = new DataUploader("localhost:8000" ,"Test id");
+        SpeedTester se = new SpeedTester(du, 1000);
+        
+        se.start();
+        
+    }
+    
+}
