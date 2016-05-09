0696f9204f1e685df949b8809d9ffbee93422d4c
hungariandude <hungariandude@users.noreply.github.com>
2016-05-09 10:08:29 +0200

    [INS-12] log4j version updated to 2.5 in probe projects

--- inetsense/inetsense-probe/inetsense-probe-logic/src/main/java/hu/elte/inetsense/probe/InetsenseProbe.java
+++ inetsense/inetsense-probe/inetsense-probe-logic/src/main/java/hu/elte/inetsense/probe/InetsenseProbe.java
@@ -1,40 +1,41 @@
 
 package hu.elte.inetsense.probe;
 
-import org.apache.log4j.Logger;
-import hu.elte.inetsense.probe.speedtester.SpeedTester;
-import hu.elte.inetsense.probe.uploader.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import hu.elte.inetsense.probe.speedtester.SpeedTester;
+import hu.elte.inetsense.probe.uploader.ConfigLoader;
+import hu.elte.inetsense.probe.uploader.DataUploader;
 
 public class InetsenseProbe {
 
-    public static Logger log = Logger.getLogger(InetsenseProbe.class.getName());
-    
-    public static void main(String[] args) {
+    public static Logger log = LogManager.getLogger();
+
+    public static void main(final String[] args) {
 
         ConfigLoader cf = null;
 
-        for(int i=0; i<args.length; i++) {
+        for (int i = 0; i < args.length; i++) {
 
-            if(args[i].equals("-config")) {
-                cf = new ConfigLoader(args[i+1]);
+            if (args[i].equals("-config")) {
+                cf = new ConfigLoader(args[i + 1]);
             }
 
         }
 
-        if(cf == null) {
+        if (cf == null) {
             cf = new ConfigLoader();
         }
-        
+
         log.info("Config loaded");
-        
-        DataUploader du = new DataUploader(cf.get("host")+":"+cf.get("port")+"/message-endpoint" ,cf.get("probe-id"));
+
+        DataUploader du = new DataUploader(cf.get("host") + ":" + cf.get("port") + "/message-endpoint", cf.get("probe-id"));
         SpeedTester se = new SpeedTester(
-          du,
-          Integer.parseInt(cf.get("test-interval")),
-          cf.get("download-location"),
-          cf.get("upload-location")
-        );
+                du,
+                Integer.parseInt(cf.get("test-interval")),
+                cf.get("download-location"),
+                cf.get("upload-location"));
 
         se.start();
 
--- inetsense/inetsense-probe/inetsense-probe-logic/src/main/java/hu/elte/inetsense/probe/uploader/DataUploader.java
+++ inetsense/inetsense-probe/inetsense-probe-logic/src/main/java/hu/elte/inetsense/probe/uploader/DataUploader.java
@@ -1,81 +1,83 @@
 package hu.elte.inetsense.probe.uploader;
 
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.io.*;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class DataUploader{
-    
-    public static Logger log = Logger.getLogger(DataUploader.class.getName());
-    
-    private String probe_id;
-    private String server_location;
+public class DataUploader {
+
+    public static Logger      log = LogManager.getLogger();
+
+    private String            probe_id;
+    private String            server_location;
     private List<Measurement> measurements;
 
-    public DataUploader(String server_location, String probe_id) {
+    public DataUploader(final String server_location, final String probe_id) {
         this.probe_id = probe_id;
-        this.server_location = "http://"+server_location;
+        this.server_location = "http://" + server_location;
         this.measurements = new ArrayList();
     }
-    
-    public void addMeasurement(Measurement me) {
-        
+
+    public void addMeasurement(final Measurement me) {
+
         this.measurements.add(me);
-        
+
         this.flush();
-        
+
     }
-    
+
     /* Todo: Check server availability */
     private void flush() {
-        
+
         /* todo: replace with a JSON lib */
-        String data = "{\"probeAuthId\":\""+probe_id+"\",\"measurements\":[";
-        
+        String data = "{\"probeAuthId\":\"" + probe_id + "\",\"measurements\":[";
+
         data += measurements.get(0).toJSON();
-        
-        for(int i=1; i<measurements.size(); i++) {
-            data += ","+measurements.get(i).toJSON();
+
+        for (int i = 1; i < measurements.size(); i++) {
+            data += "," + measurements.get(i).toJSON();
         }
-        
+
         data += "]}";
-        
+
         try {
             URL url = new URL(server_location);
-            
-            byte[] postData       = data.getBytes( StandardCharsets.UTF_8 );
-            int    postDataLength = postData.length;
-            
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();        
-            conn.setDoOutput( true );
-            conn.setInstanceFollowRedirects( false );
-            conn.setRequestMethod( "POST" );
-            conn.setRequestProperty( "Content-Type", "text/json"); 
-            conn.setRequestProperty( "Charset", "utf-8");
-            conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-            
-            DataOutputStream wr= new DataOutputStream(conn.getOutputStream());
+
+            byte[] postData = data.getBytes(StandardCharsets.UTF_8);
+            int postDataLength = postData.length;
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "text/json");
+            conn.setRequestProperty("Charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
             wr.write(postData);
             wr.write('\n');
-            
+
             wr.flush();
             wr.close();
-            
-            log.info("DATA sent! Response code: "+conn.getResponseCode());
-            
+
+            log.info("DATA sent! Response code: " + conn.getResponseCode());
+
             this.measurements.clear();
-            
+
             conn.disconnect();
-        
-        } catch(Exception ex) {
+
+        } catch (Exception ex) {
             // print error, try again later
             log.error(ex.getMessage());
         }
-        
+
     }
-    
+
 }
