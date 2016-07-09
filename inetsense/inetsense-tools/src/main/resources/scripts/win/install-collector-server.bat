3dd519b34fcccec49b51b464032baee466e30189
Bodó Alex <alex.bodo@gmail.com>
2016-07-09 20:27:31 +0200

    probe starts with java ws

--- inetsense/inetsense-tools/src/main/resources/scripts/win/install-collector-server.bat
+++ inetsense/inetsense-tools/src/main/resources/scripts/win/install-collector-server.bat
@@ -1,5 +1,15 @@
 
 call setvars.bat
 
+cd ..\..\dist\static\jnlp
+
+%JAVA_HOME%\bin\jarsigner -tsa http://timestamp.digicert.com -keystore keystore.jks -storepass server inetsense-probe-desktop-${project.version}.jar server
+
+cd ..\..
+
+%JAVA_HOME%\bin\jar uf inetsense-server-collector-${project.version}.jar static\jnlp\inetsense-probe-desktop-${project.version}.jar
+
+cd ..\scripts\win
+
 xcopy /y ..\..\dist\config\collector-server.properties %INETSENSE_INSTALL_DIR%\\%INETSENSE_COLLECTOR_DIR_NAME%\
 xcopy /y ..\..\dist\inetsense-server-collector-${project.version}.jar %INETSENSE_INSTALL_DIR%\\%INETSENSE_COLLECTOR_DIR_NAME%\
