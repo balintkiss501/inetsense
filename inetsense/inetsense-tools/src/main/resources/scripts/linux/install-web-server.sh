123646f25dcb84b4a28e69f86e34027d5c172cea
Zoltan Daniel Torok <dzsig.92@gmail.com>
2016-10-18 16:00:22 +0200

    fixed install-web-server.sh

--- inetsense/inetsense-tools/src/main/resources/scripts/linux/install-web-server.sh
+++ inetsense/inetsense-tools/src/main/resources/scripts/linux/install-web-server.sh
@@ -2,7 +2,7 @@
 
 . setvars.sh
 
-mkdir -p $INETSENSE_INSTALL_DIR/$INETSENSE_COLLECTOR_DIR_NAME
+mkdir -p $INETSENSE_INSTALL_DIR/$INETSENSE_WEB_DIR_NAME
 
 cp ../../dist/config/web-server.properties $INETSENSE_INSTALL_DIR/$INETSENSE_WEB_DIR_NAME/
 cp ../../dist/inetsense-server-web-${project.version}.jar $INETSENSE_INSTALL_DIR/$INETSENSE_WEB_DIR_NAME/
