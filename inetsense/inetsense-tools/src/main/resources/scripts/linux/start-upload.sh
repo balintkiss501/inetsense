c60680c8849041f012bd6e4d142f58b18408fdf0
BODO <U527532@ads.dlh.de>
2016-12-01 19:00:28 +0100

    upload sh test2

--- inetsense/inetsense-tools/src/main/resources/scripts/linux/start-upload.sh
+++ inetsense/inetsense-tools/src/main/resources/scripts/linux/start-upload.sh
@@ -5,7 +5,7 @@
 cd $INETSENSE_INSTALL_DIR/$INETSENSE_UPLOAD_DIR_NAME
 
 echo "================================="
-echo UPLOAD_JAVA_OPT
+echo $UPLOAD_JAVA_OPT
 echo "================================="
 
 $JAVA_HOME/bin/java $UPLOAD_JAVA_OPT -jar $INETSENSE_INSTALL_DIR/$INETSENSE_UPLOAD_DIR_NAME/inetsense-server-upload-${project.version}.jar
